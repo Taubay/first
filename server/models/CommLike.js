47d8f212462bb8c6be859724fd1a77a0d3950d39
Taubay <taubay.kuzh.00@mail.ru>
2017-08-21 17:12:51 +0000

    CheckTBOT

--- server/models/CommLike.js
+++ server/models/CommLike.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 var CommLikeSchema = new mongoose.Schema({
-    comment : {type: mongoose.Schema.ObjectId , ref:"Comment"} ,
+    comment : {type: mongoose.Schema.ObjectId ,ref:"Comment"} ,
     user : {type: mongoose.Schema.ObjectId , ref:"User"} ,
     date : {type:Date, default : Date.now}})
 module.exports = mongoose.model("CommLike", CommLikeSchema);
